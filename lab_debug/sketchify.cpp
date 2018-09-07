8330242c31d8550d4e771956bd1134173afa1cae
Sukmin Hwang <SukMin@wirelessprv-10-195-215-64.near.illinois.edu>
2018-09-07 18:49:23 -0500

    file submission

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -12,30 +12,33 @@ PNG* setupOutput(unsigned w, unsigned h) {
 }
 
 // Returns my favorite color
-HSLAPixel* myFavoriteColor(double hue) {
-  HSLAPixel p(hue, 0.8, 0.5);
-  return &p;
+HSLAPixel* myFavoriteColor(double hue)
+ {
+  HSLAPixel *p = new HSLAPixel(hue, 0.8, 0.5); //err
+
+  return p; //err
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-
+    PNG* original = new PNG();
+cout << "Reached line 23" << endl;
     original->readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
 
     // Create out.png
     PNG* output;
-    setupOutput(width, height);
+    output = setupOutput(width, height); //err
+
 
     // Load our favorite color to color the outline
-    HSLAPixel* myPixel = myFavoriteColor(-1);
+    HSLAPixel* myPixel = myFavoriteColor(26); //err
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1;  y < height; y++) { //err
+        for (unsigned x = 1; x < width; x++) {
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
@@ -43,7 +46,7 @@ void sketchify(std::string inputFile, std::string outputFile) {
 
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
+            HSLAPixel & currOutPixel = (*output).getPixel(x, y);//err
             if (diff > 20) {
                 currOutPixel = *myPixel;
             }
@@ -54,7 +57,9 @@ void sketchify(std::string inputFile, std::string outputFile) {
     output->writeToFile(outputFile);
 
     // Clean up memory
-    delete myPixel;
+    //delete myPixel;
+
     delete output;
     delete original;
+  ;
 }

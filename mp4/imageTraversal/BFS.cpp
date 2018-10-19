a6132a95b1d3a890ddf79aeebc9f01a8b80a4fc3
Sukmin Hwang <SukMin@wirelessprv-10-192-242-221.near.illinois.edu>
2018-10-18 21:37:27 -0500

    file submission

--- mp4/imageTraversal/BFS.cpp
+++ mp4/imageTraversal/BFS.cpp
@@ -1,204 +1,204 @@
-// #include <iterator>
-// #include <cmath>
-//
-// #include <list>
-// #include <queue>
-// #include <stack>
-// #include <vector>
-//
-// #include "../cs225/PNG.h"
-// #include "../Point.h"
-//
-// #include "ImageTraversal.h"
-// #include "BFS.h"
-//
-// using namespace std;
-//
-// using namespace cs225;
-//
-// /**
-//  * Initializes a breadth-first ImageTraversal on a given `png` image,
-//  * starting at `start`, and with a given `tolerance`.
-//  * @param png The image this BFS is going to traverse
-//  * @param start The start point of this BFS
-//  * @param tolerance If the current point is too different (difference larger than tolerance) with the start point,
-//  * it will not be included in this BFS
-//  */
-// BFS::BFS(const PNG & png, const Point & start, double tolerance)
-// //  : png_(png), start_(start), tolerance_(tolerance)
-//  {
-//   /** @todo [Part 1] */
-//   png_ = png;
-//   start_ = start;
-//   tolerance_ = tolerance;
-//
-//   queue_.push(start_);
-//
-//    //height_ = png_.height();
-//    //width_ = png_.width();
-//
-//    occupied = new int*[png.width()];
-//
-//
-// //  occupied.resize(width_, vector<bool>(height_));
-//
-//   for(unsigned i = 0; i < png_.width(); i++){
-//     occupied[i] = new int [png_.height()];
-//     for(unsigned j = 0; j < png_.height(); j++){
-//       occupied[i][j] = 0; // false.
-//     }
-//   }
-// //  occupied[start_x_,start_y_] = true;
-//
-// }
-//
-// /**
-//  * Returns an iterator for the traversal starting at the first point.
-//  */
-// ImageTraversal::Iterator BFS::begin() {
-//   /** @todo [Part 1] */
-//   ImageTraversal * begin = new BFS(png_, start_, tolerance_);
-//   return ImageTraversal::Iterator(begin);
-// }
-//
-// /**
-//  * Returns an iterator for the traversal one past the end of the traversal.
-//  */
-// ImageTraversal::Iterator BFS::end() {
-//   /** @todo [Part 1] */
-//   return ImageTraversal::Iterator();
-// }
-//
-// /**
-//  * Adds a Point for the traversal to visit at some point in the future.
-//  */
-// void BFS::add(const Point & point) {
-//   /** @todo [Part 1] */
-//   // unsigned int x_ = point.x;
-//   // unsigned int y_ = point.y;
-//   // double toler = calculateDelta(png_.getPixel(current_.x+1,current_.y), png_.getPixel(start_.x,start_.y));
-//   // bool tolerance_check_ =  toler < tolerance_ ? true : false;
-//   //
-//   // if( x_+1 < width_ && (occupied[x_+1][y_]==0) && tolerance_check_){
-//   //     // queue_.push( Point(x_+1, y_));
-//   //     // occupied[x_+1,y_] = 1;
-//   //     Point right(x_+1, y_);
-//   //     queue_.push(right);
-//   // //    occupied[x_+1][y_]=1;
-//   // }
-//   //
-//   // toler = calculateDelta(png_.getPixel(current_.x,current_.y+1), png_.getPixel(start_.x,start_.y));
-//   // tolerance_check_ =  toler < tolerance_ ? true : false;
-//   //  if( y_+1 < height_ && (occupied[x_][y_+1]==0) && tolerance_check_){
-//   //     // queue_.push( Point(x_,y_+1));
-//   //     // occupied[x_,y_+1] = 1;
-//   //     Point down(x_,y_+1);
-//   //     queue_.push(down);
-//   // //    occupied[x_][y_+1]=1;
-//   // }
-//   //
-//   // toler = calculateDelta(png_.getPixel(current_.x-1,current_.y), png_.getPixel(start_.x,start_.y));
-//   // tolerance_check_ =  toler < tolerance_ ? true : false;
-//   //  if( x_ > 0 && x_+1 <= width_ && (occupied[x_-1][y_]==0) && tolerance_check_){
-//   //      // queue_.push( Point(x_,y_+1));
-//   //      // occupied[x_,y_+1] = 1;
-//   //      Point left(x_-1,y_);
-//   //      queue_.push(left);
-//   //   //   occupied[x_-1][y_]=1;
-//   //  }
-//   //  toler = calculateDelta(png_.getPixel(current_.x,current_.y-1), png_.getPixel(start_.x,start_.y));
-//   //  tolerance_check_ =  toler < tolerance_ ? true : false;
-//   //   if( y_ > 0 && y_+1 <= height_ && (occupied[x_][y_-1]==0) && tolerance_check_){
-//   //       // queue_.push( Point(x_,y_+1));
-//   //       // occupied[x_,y_+1] = 1;
-//   //       Point up(x_,y_-1);
-//   //       queue_.push(up);
-//   // //      occupied[x_][y_+1]=1;
-//   //   }
-//
-// if(point.x < (png_.width()-1)){
-//  double cur_tolerance_ = calculateDelta(png_.getPixel(point.x+1, point.y), png_.getPixel(start_.x, start_.y));
-//
-//  if(cur_tolerance_ < tolerance_ && occupied[point.x+1][point.y] == 0){
-//    Point east(point.x+1, point.y);
-//    queue_.push(east);
-//   //    occupied[point.x+1][point.y]=1;
-//  }
-// }
-//
-// if(point.y < (png_.height() -1)){
-//  double cur_tolerance_ = calculateDelta(png_.getPixel(point.x, point.y+1), png_.getPixel(start_.x, start_.y));
-//
-//  if(cur_tolerance_< tolerance_ && occupied[point.x][point.y+1] == 0){
-//    Point south(point.x, point.y+1);
-//    queue_.push(south);
-//   //    occupied[point.x][point.y+1]=1;
-//  }
-// }
-//
-// if(point.x > 0 && point.x <= (png_.width()-1) ){
-//  double cur_tolerance_ = calculateDelta(png_.getPixel(point.x-1, point.y), png_.getPixel(start_.x, start_.y));
-//
-//  if(cur_tolerance_ < tolerance_ && occupied[point.x-1][point.y] == 0){
-//    Point west(point.x-1, point.y);
-//    queue_.push(west);
-//   //    occupied[point.x-1][point.y]=1;
-//  }
-// }
-//
-// if(point.y > 0 && point.y <= (png_.height() -1)){
-//  double cur_tolerance_ = calculateDelta(png_.getPixel(point.x, point.y-1), png_.getPixel(start_.x, start_.y));
-//
-//  if(cur_tolerance_ < tolerance_ && occupied[point.x][point.y-1] == 0){
-//    Point north(point.x, point.y -1);
-//    queue_.push(north);
-// //   occupied[point.x][point.y-1]=1;
-//
-//  }
-// }
-//
-// }
-//
-// /**
-//  * Removes and returns the current Point in the traversal.
-//  */
-// Point BFS::pop() {
-//   /** @todo [Part 1] */
-// //  return Point(0, 0);
-//   Point temp = queue_.front();
-//   queue_.pop();
-//   return temp;
-// }
-//
-// /**
-//  * Returns the current Point in the traversal.
-//  */
-// Point BFS::peek() const {
-//   /** @todo [Part 1] */
-//   if(!empty()){
-//       return queue_.front();
-//   }else{
-//     return Point(0,0);
-//   }
-//
-// }
-//
-// /**
-//  * Returns true if the traversal is empty.
-//  */
-// bool BFS::empty() const {
-//   /** @todo [Part 1] */
-//   if(queue_.empty()){
-//     return true;
-//   }else{
-//     return false;
-//   }
-//   //return (queue_.empty());
-// }
-//
-// BFS::~BFS(){
-//   for(unsigned i=0; i<png_.width(); i++){
-//     delete occupied[i];
-//   }
-//   delete occupied;
-// }
+#include <iterator>
+#include <cmath>
+
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+
+#include "../cs225/PNG.h"
+#include "../Point.h"
+
+#include "ImageTraversal.h"
+#include "BFS.h"
+
+using namespace std;
+
+using namespace cs225;
+
+/**
+ * Initializes a breadth-first ImageTraversal on a given `png` image,
+ * starting at `start`, and with a given `tolerance`.
+ * @param png The image this BFS is going to traverse
+ * @param start The start point of this BFS
+ * @param tolerance If the current point is too different (difference larger than tolerance) with the start point,
+ * it will not be included in this BFS
+ */
+BFS::BFS(const PNG & png, const Point & start, double tolerance)
+//  : png_(png), start_(start), tolerance_(tolerance)
+ {
+  /** @todo [Part 1] */
+  png_ = png;
+  start_ = start;
+  tolerance_ = tolerance;
+
+  queue_.push(start_);
+
+   //height_ = png_.height();
+   //width_ = png_.width();
+
+   occupied = new int*[png.width()];
+
+
+//  occupied.resize(width_, vector<bool>(height_));
+
+  for(unsigned i = 0; i < png_.width(); i++){
+    occupied[i] = new int [png_.height()];
+    for(unsigned j = 0; j < png_.height(); j++){
+      occupied[i][j] = 0; // false.
+    }
+  }
+//  occupied[start_x_,start_y_] = true;
+
+}
+
+/**
+ * Returns an iterator for the traversal starting at the first point.
+ */
+ImageTraversal::Iterator BFS::begin() {
+  /** @todo [Part 1] */
+  ImageTraversal * begin = new BFS(png_, start_, tolerance_);
+  return ImageTraversal::Iterator(begin);
+}
+
+/**
+ * Returns an iterator for the traversal one past the end of the traversal.
+ */
+ImageTraversal::Iterator BFS::end() {
+  /** @todo [Part 1] */
+  return ImageTraversal::Iterator();
+}
+
+/**
+ * Adds a Point for the traversal to visit at some point in the future.
+ */
+void BFS::add(const Point & point) {
+  /** @todo [Part 1] */
+  // unsigned int x_ = point.x;
+  // unsigned int y_ = point.y;
+  // double toler = calculateDelta(png_.getPixel(current_.x+1,current_.y), png_.getPixel(start_.x,start_.y));
+  // bool tolerance_check_ =  toler < tolerance_ ? true : false;
+  //
+  // if( x_+1 < width_ && (occupied[x_+1][y_]==0) && tolerance_check_){
+  //     // queue_.push( Point(x_+1, y_));
+  //     // occupied[x_+1,y_] = 1;
+  //     Point right(x_+1, y_);
+  //     queue_.push(right);
+  // //    occupied[x_+1][y_]=1;
+  // }
+  //
+  // toler = calculateDelta(png_.getPixel(current_.x,current_.y+1), png_.getPixel(start_.x,start_.y));
+  // tolerance_check_ =  toler < tolerance_ ? true : false;
+  //  if( y_+1 < height_ && (occupied[x_][y_+1]==0) && tolerance_check_){
+  //     // queue_.push( Point(x_,y_+1));
+  //     // occupied[x_,y_+1] = 1;
+  //     Point down(x_,y_+1);
+  //     queue_.push(down);
+  // //    occupied[x_][y_+1]=1;
+  // }
+  //
+  // toler = calculateDelta(png_.getPixel(current_.x-1,current_.y), png_.getPixel(start_.x,start_.y));
+  // tolerance_check_ =  toler < tolerance_ ? true : false;
+  //  if( x_ > 0 && x_+1 <= width_ && (occupied[x_-1][y_]==0) && tolerance_check_){
+  //      // queue_.push( Point(x_,y_+1));
+  //      // occupied[x_,y_+1] = 1;
+  //      Point left(x_-1,y_);
+  //      queue_.push(left);
+  //   //   occupied[x_-1][y_]=1;
+  //  }
+  //  toler = calculateDelta(png_.getPixel(current_.x,current_.y-1), png_.getPixel(start_.x,start_.y));
+  //  tolerance_check_ =  toler < tolerance_ ? true : false;
+  //   if( y_ > 0 && y_+1 <= height_ && (occupied[x_][y_-1]==0) && tolerance_check_){
+  //       // queue_.push( Point(x_,y_+1));
+  //       // occupied[x_,y_+1] = 1;
+  //       Point up(x_,y_-1);
+  //       queue_.push(up);
+  // //      occupied[x_][y_+1]=1;
+  //   }
+
+if(point.x < (png_.width()-1)){
+ double cur_tolerance_ = calculateDelta(png_.getPixel(point.x+1, point.y), png_.getPixel(start_.x, start_.y));
+
+ if(cur_tolerance_ < tolerance_ && occupied[point.x+1][point.y] == 0){
+   Point east(point.x+1, point.y);
+   queue_.push(east);
+  //    occupied[point.x+1][point.y]=1;
+ }
+}
+
+if(point.y < (png_.height() -1)){
+ double cur_tolerance_ = calculateDelta(png_.getPixel(point.x, point.y+1), png_.getPixel(start_.x, start_.y));
+
+ if(cur_tolerance_< tolerance_ && occupied[point.x][point.y+1] == 0){
+   Point south(point.x, point.y+1);
+   queue_.push(south);
+  //    occupied[point.x][point.y+1]=1;
+ }
+}
+
+if(point.x > 0 && point.x <= (png_.width()-1) ){
+ double cur_tolerance_ = calculateDelta(png_.getPixel(point.x-1, point.y), png_.getPixel(start_.x, start_.y));
+
+ if(cur_tolerance_ < tolerance_ && occupied[point.x-1][point.y] == 0){
+   Point west(point.x-1, point.y);
+   queue_.push(west);
+  //    occupied[point.x-1][point.y]=1;
+ }
+}
+
+if(point.y > 0 && point.y <= (png_.height() -1)){
+ double cur_tolerance_ = calculateDelta(png_.getPixel(point.x, point.y-1), png_.getPixel(start_.x, start_.y));
+
+ if(cur_tolerance_ < tolerance_ && occupied[point.x][point.y-1] == 0){
+   Point north(point.x, point.y -1);
+   queue_.push(north);
+//   occupied[point.x][point.y-1]=1;
+
+ }
+}
+
+}
+
+/**
+ * Removes and returns the current Point in the traversal.
+ */
+Point BFS::pop() {
+  /** @todo [Part 1] */
+//  return Point(0, 0);
+  Point temp = queue_.front();
+  queue_.pop();
+  return temp;
+}
+
+/**
+ * Returns the current Point in the traversal.
+ */
+Point BFS::peek() const {
+  /** @todo [Part 1] */
+  if(!empty()){
+      return queue_.front();
+  }else{
+    return Point(0,0);
+  }
+
+}
+
+/**
+ * Returns true if the traversal is empty.
+ */
+bool BFS::empty() const {
+  /** @todo [Part 1] */
+  if(queue_.empty()){
+    return true;
+  }else{
+    return false;
+  }
+  //return (queue_.empty());
+}
+
+BFS::~BFS(){
+  for(unsigned i=0; i<png_.width(); i++){
+    delete occupied[i];
+  }
+  delete occupied;
+}
--- mp4/imageTraversal/ImageTraversal.cpp
+++ mp4/imageTraversal/ImageTraversal.cpp
@@ -1,114 +1,114 @@
-// #include <cmath>
-// #include <iterator>
-// #include <iostream>
-//
-// #include "../cs225/HSLAPixel.h"
-// #include "../cs225/PNG.h"
-// #include "../Point.h"
-//
-// #include "ImageTraversal.h"
-//
-// /**
-//  * Calculates a metric for the difference between two pixels, used to
-//  * calculate if a pixel is within a tolerance.
-//  *
-//  * @param p1 First pixel
-//  * @param p2 Second pixel
-//  * @return the difference between two HSLAPixels
-//  */
-// double ImageTraversal::calculateDelta(const HSLAPixel & p1, const HSLAPixel & p2) {
-//   double h = fabs(p1.h - p2.h);
-//   double s = p1.s - p2.s;
-//   double l = p1.l - p2.l;
-//
-//   // Handle the case where we found the bigger angle between two hues:
-//   if (h > 180) { h = 360 - h; }
-//   h /= 360;
-//
-//   return sqrt( (h*h) + (s*s) + (l*l) );
-// }
-//
-// /**
-//  * Default iterator constructor.
-//  */
-// ImageTraversal::Iterator::Iterator() {
-//   /** @todo [Part 1] */
-//     temp = NULL;
-// }
-//
-// ImageTraversal::Iterator::Iterator(ImageTraversal* tempo) {
-//   /** @todo [Part 1] */
-//     temp = tempo;
-//     curr = temp->start_;
-// }
-//
-// /**
-//  * Iterator increment opreator.
-//  *
-//  * Advances the traversal of the image.
-//  */
-// ImageTraversal::Iterator & ImageTraversal::Iterator::operator++() {
-//   /** @todo [Part 1] */
-//   // if(!temp -> empty()){
-//   //     curr = temp->pop();
-//   //     temp -> add(curr);
-//   //     curr = temp->peek();
-//   // }
-//   if(!temp->empty()){
-//     curr = temp->pop();
-//     temp->occupied[curr.x][curr.y] = 1;
-//     while(!temp->empty() && temp->occupied[temp->peek().x][temp->peek().y] == 1)
-//       Point junk = temp->pop();
-//     temp->add(curr);
-//
-//     if(temp->empty())
-//       temp = NULL;
-//     else
-//       curr = temp->peek();
-//     }
-//   return *this;
-//   //return *this;
-// }
-//
-// /**
-//  * Iterator accessor opreator.
-//  *
-//  * Accesses the current Point in the ImageTraversal.
-//  */
-// Point ImageTraversal::Iterator::operator*() {
-//   /** @todo [Part 1] */
-//   return temp -> peek();
-//   //return curr;
-// }
-//
-// /**
-//  * Iterator inequality operator.
-//  *
-//  * Determines if two iterators are not equal.
-//  */
-// bool ImageTraversal::Iterator::operator!=(const ImageTraversal::Iterator &other) {
-//   /** @todo [Part 1] */
-//   bool compare1 = (temp == nullptr) ? true : false;
-//   bool compare2 = (other.temp == nullptr) ? true : false;
-//
-//   compare1 = (compare1 == true) ? compare1 : temp->empty();
-//   compare2 = (compare2 == true) ? compare2 : other.temp->empty();
-//
-//   if(compare1 && compare2){return false;}
-//   else if( !compare1 && !compare2){return temp != other.temp;}
-//   else {return true;}
-//
-//   // bool thisEmpty = false;
-//   // bool otherEmpty = false;
-//   //
-//   // if (temp == NULL) { thisEmpty = true; }
-//   // if (other.temp == NULL) { otherEmpty = true; }
-//   //
-//   // if (!thisEmpty)  { thisEmpty = temp->empty(); }
-//   // if (!otherEmpty) { otherEmpty = other.temp->empty(); }
-//   //
-//   // if (thisEmpty && otherEmpty) return false; // both empty then the traversals are equal, return true
-//   // else if ((!thisEmpty)&&(!otherEmpty)) return (temp != other.temp); //both not empty then compare the traversals
-//   // else return true; // one is empty while the other is not, return true
-//
-// }
+#include <cmath>
+#include <iterator>
+#include <iostream>
+
+#include "../cs225/HSLAPixel.h"
+#include "../cs225/PNG.h"
+#include "../Point.h"
+
+#include "ImageTraversal.h"
+
+/**
+ * Calculates a metric for the difference between two pixels, used to
+ * calculate if a pixel is within a tolerance.
+ *
+ * @param p1 First pixel
+ * @param p2 Second pixel
+ * @return the difference between two HSLAPixels
+ */
+double ImageTraversal::calculateDelta(const HSLAPixel & p1, const HSLAPixel & p2) {
+  double h = fabs(p1.h - p2.h);
+  double s = p1.s - p2.s;
+  double l = p1.l - p2.l;
+
+  // Handle the case where we found the bigger angle between two hues:
+  if (h > 180) { h = 360 - h; }
+  h /= 360;
+
+  return sqrt( (h*h) + (s*s) + (l*l) );
+}
+
+/**
+ * Default iterator constructor.
+ */
+ImageTraversal::Iterator::Iterator() {
+  /** @todo [Part 1] */
+    temp = NULL;
+}
+
+ImageTraversal::Iterator::Iterator(ImageTraversal* tempo) {
+  /** @todo [Part 1] */
+    temp = tempo;
+    curr = temp->start_;
+}
+
+/**
+ * Iterator increment opreator.
+ *
+ * Advances the traversal of the image.
+ */
+ImageTraversal::Iterator & ImageTraversal::Iterator::operator++() {
+  /** @todo [Part 1] */
+  // if(!temp -> empty()){
+  //     curr = temp->pop();
+  //     temp -> add(curr);
+  //     curr = temp->peek();
+  // }
+  if(!temp->empty()){
+    curr = temp->pop();
+    temp->occupied[curr.x][curr.y] = 1;
+    while(!temp->empty() && temp->occupied[temp->peek().x][temp->peek().y] == 1)
+      Point junk = temp->pop();
+    temp->add(curr);
+
+    if(temp->empty())
+      temp = NULL;
+    else
+      curr = temp->peek();
+    }
+  return *this;
+  //return *this;
+}
+
+/**
+ * Iterator accessor opreator.
+ *
+ * Accesses the current Point in the ImageTraversal.
+ */
+Point ImageTraversal::Iterator::operator*() {
+  /** @todo [Part 1] */
+  return temp -> peek();
+  //return curr;
+}
+
+/**
+ * Iterator inequality operator.
+ *
+ * Determines if two iterators are not equal.
+ */
+bool ImageTraversal::Iterator::operator!=(const ImageTraversal::Iterator &other) {
+  /** @todo [Part 1] */
+  bool compare1 = (temp == nullptr) ? true : false;
+  bool compare2 = (other.temp == nullptr) ? true : false;
+
+  compare1 = (compare1 == true) ? compare1 : temp->empty();
+  compare2 = (compare2 == true) ? compare2 : other.temp->empty();
+
+  if(compare1 && compare2){return false;}
+  else if( !compare1 && !compare2){return temp != other.temp;}
+  else {return true;}
+
+  // bool thisEmpty = false;
+  // bool otherEmpty = false;
+  //
+  // if (temp == NULL) { thisEmpty = true; }
+  // if (other.temp == NULL) { otherEmpty = true; }
+  //
+  // if (!thisEmpty)  { thisEmpty = temp->empty(); }
+  // if (!otherEmpty) { otherEmpty = other.temp->empty(); }
+  //
+  // if (thisEmpty && otherEmpty) return false; // both empty then the traversals are equal, return true
+  // else if ((!thisEmpty)&&(!otherEmpty)) return (temp != other.temp); //both not empty then compare the traversals
+  // else return true; // one is empty while the other is not, return true
+
+}
